a5f074136a3c5c1d3975b879d2ecd62878ffaa33
BelvaLA <113889641+BelvaLA@users.noreply.github.com>
2022-09-22 08:57:26 +0700

    Update cbcb.py

--- cbcb.py
+++ cbcb.py
@@ -1,9 +1,19 @@
-intro = input("Hi user, siapa namamu? ")
-print(f"Hi {intro}, Program ini dibuat untuk membantu anda menentukan luas ruangan anda")
+"""
+Created on wednesday 21 sept 2022
+@author : Belva Luthfiah Andini
+@author NIM : 065002200035
 
+This code is for Tugas 2 Latihan Praktikum Algo
+"""
+#code ini untuk menanyakan uname 
+intro = input("Hi user selamat datang diprogram kami, siapa namamu? ")
+print(f"Hi {intro}, Program ini dibuat untuk membantu anda menentukan luas ruangan anda") 
+
+#code ini menginput nilai / data yang user berikan
 P = int(input("Silahkan masukan panjang ruangan: "))
 L = int(input("Silahkan masukan Lebar ruangan: "))
-Satuan = input("Masukan satuan yang diinginkan Meter/Inci: ")
-Jumlah = P * L
+Satuan = input("Masukan satuan yang diinginkan Meter/Inch: ")
+Luas = P * L 
 
-print(f"Hasil dari luas ruangan anda adalah {Jumlah} {Satuan} ")
+#code ini memberikan output dari hasil nilai / data yg user berikan
+print(f"Hasil dari luas ruangan anda adalah {Luas} {Satuan} ")
